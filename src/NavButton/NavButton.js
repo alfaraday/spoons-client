e9978cbdaef04405e90efc67ca87173a598dcc0d
Ariana Faraday <ariana@thinkful.com>
2019-11-08 08:05:21 -0500

    template for each component

---
+++ src/NavButton/NavButton.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class NavButton extends Component {
+  render() {
+    return <button>Under construction</button>
+  }
+}
---
+++ src/Tag/Tag.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Tag extends Component {
+  render() {
+    return <button>Under construction</button>
+  }
+}
