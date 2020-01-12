11dfabc807d6c6660a2533614b8f47d90195683f
Ariana Faraday <ariana@thinkful.com>
2020-01-12 12:56:13 -0500

    LandingPage structure

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Nav from '../Nav/Nav';
+import SearchBox from '../SearchBox/SearchBox'
+import Tag from '../Tag/Tag'
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <main>
+          <div>
+            <h1>SPOONS</h1>
+            <p>Trouble deciding what to eat? Save your spoons for bigger problems! Collect all your favorite recipes here and never struggle over food decisions again.</p>
+          </div>
+          <SearchBox />
+          <Tag />
+        </main>
+      </div>
+    )
+  }
+}
