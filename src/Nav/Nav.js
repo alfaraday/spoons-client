55714bb9ac0cee0a88343feeb9ffbdc0c163569c
Ariana Faraday <ariana@thinkful.com>
2020-01-12 13:21:31 -0500

    Nav structure

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 export default class Nav extends Component {
   render() {
-    return <nav>Under construction</nav>
+    return <nav>
+      <ul>
+        <li>SPOONS</li>
+        <li>+</li>
+        <li>Home</li>
+      </ul>
+    </nav>
   }
 }
