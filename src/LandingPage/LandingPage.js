ab2e0283870c3580ed30338365567382cdb05b11
Ariana Faraday <ariana@thinkful.com>
2020-01-13 18:43:23 -0500

    Tag to Tags

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
 import SearchBox from '../SearchBox/SearchBox'
-import Tag from '../Tags/Tags'
+import Tags from '../Tags/Tags'
 
 export default class LandingPage extends Component {
   render() {
@@ -14,7 +14,7 @@ export default class LandingPage extends Component {
             <p>Trouble deciding what to eat? Save your spoons for bigger problems! Collect all your favorite recipes here and never struggle over food decisions again.</p>
           </div>
           <SearchBox />
-          <Tag />
+          <Tags />
         </main>
       </div>
     )
